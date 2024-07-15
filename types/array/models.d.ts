6028c2ae2c0cde0a87dd8eb9f42a77591cc95fd5
Oscar Palmér <opalmer@me.com>
2024-07-15 10:50:54 +0200

    0.59.0 - package json lint + export fixes

--- types/array/models.d.ts
+++ types/array/models.d.ts
@@ -1,13 +1,13 @@
 import type { Key } from '../models';
-export type BooleanCallback<Value> = GenericCallback<Value, boolean>;
+export type ArrayCallback<Value, Returned> = (value: Value, index: number, array: Value[]) => Returned;
+export type BooleanCallback<Value> = ArrayCallback<Value, boolean>;
 export type Callbacks<Value> = {
     bool?: BooleanCallback<Value>;
     key?: KeyCallback<Value>;
 };
 export type FindType = 'index' | 'value';
-export type GenericCallback<Value, Returned> = (value: Value, index: number, array: Value[]) => Returned;
 export type InsertType = 'push' | 'splice';
-export type KeyCallback<Value> = GenericCallback<Value, Key>;
+export type KeyCallback<Value> = ArrayCallback<Value, Key>;
 export type SortKey<Value> = {
     direction: 'asc' | 'desc';
     value: Key | SortKeyCallback<Value>;
